f6648d30e1c092b40687c83e78b631fce8d98293
aleker <aleksandra.kerebinska@student.put.poznan.pl>
2018-04-07 19:33:32 +0200

    Small improvements.

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -24,7 +24,8 @@ int main(int argc, char *argv[]) {
     int loopsCount = 4;
     while(loopsCount > 0) {
         test.increment();
-        std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::chrono::seconds sec(2);
+        std::this_thread::sleep_for(sec);
         loopsCount--;
     }
 
