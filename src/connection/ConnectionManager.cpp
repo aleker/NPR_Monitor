74da9d81e36c235e5a21f85bd1835cf940b1639e
aleker <aleksandra.kerebinska@student.put.poznan.pl>
2018-04-27 22:16:26 +0200

    TODO in CMakeLists

---
+++ src/connection/ConnectionManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ola on 27.04.18.
+//
+
+#include "ConnectionManager.h"
---
+++ src/connection/ConnectionManager.h
@@ -0,0 +1,14 @@
+//
+// Created by ola on 27.04.18.
+//
+
+#ifndef NPR_MONITOR_CONNECTIONMANAGER_H
+#define NPR_MONITOR_CONNECTIONMANAGER_H
+
+
+class ConnectionManager {
+
+};
+
+
+#endif //NPR_MONITOR_CONNECTIONMANAGER_H
